1735ccb76c69bdc8f9f33fd948b0894215e3e7a8
Tejasvani <83341625+Tejasvani8@users.noreply.github.com>
2021-10-10 20:39:58 +0530

    Update churn.py

--- Homework #5/churn.py
+++ Homework #5/churn.py
@@ -1,34 +1,34 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+
 
 import pickle 
 from flask import Flask 
 from flask import request 
 from flask import jsonify 
-model_file = 'Churn_model=1.0.bin'
 
+dv_file = 'dv.bin'
+model_file = 'model1.bin'
 
-with open(model_file,'rb') as f_in:
-    dv,model =  pickle.load(f_in)
+with open(model_file, 'rb') as f:
+    model = pickle.load(f)
 
+with open(dv_file, 'rb') as f:
+    dv = pickle.load(f)
+    
 app = Flask('churn')
-@app.route('/predict',methods =['POST'])
 
+@app.route('/predict', methods=['POST'])
 def predict():
     customer = request.get_json()
-
-    X = dv.transform([customer])
-    y_pred = model.predict_proba(X)[:,1]
-    churn = y_pred >= 0.5
+    X = dv.transform(customer)
+    prediction = model.predict_proba(X)[0,1]
+    churn = prediction >= 0.5
+    
     result = {
-        'churn_probability' : float(y_pred ),
-        'churn' :bool(churn)
-
+        "churn_probability": float(prediction),
+        "churn": bool(churn)
     }
     
     return jsonify(result)
 
- 
-
-if __name__=="__main__":
-    app.run(debug =True,host = '0.0.0.0',port = 9696)
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=9696)
